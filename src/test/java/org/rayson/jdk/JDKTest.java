bdd2fa0e215cb1f1f6ca1be5d44bee6ca6366168
lirui <634178463@qq.com>
2018-08-30 17:48:48 +0800

    Test jdk for "_"

---
+++ src/test/java/org/rayson/jdk/JDKTest.java
@@ -0,0 +1,21 @@
+package org.rayson.jdk;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/***
+ *  Created with IntelliJ IDEA.
+ *  User:  lirui
+ *  Date:  2018-08-30
+ *  Time: 下午2:45
+ *  Description:
+ **/
+public class JDKTest {
+
+    @Test
+    public void test_() {
+        long amount = 10_000_000L;
+        assertEquals(10000000L, amount);
+    }
+}
