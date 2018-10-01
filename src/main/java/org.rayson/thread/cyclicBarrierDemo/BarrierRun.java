32e450b6654da8dbe23ad9905448737af8047516
lirui <634178463@qq.com>
2018-10-01 18:41:12 +0800

    CyclicBarrier

---
+++ src/main/java/org.rayson/thread/cyclicBarrierDemo/BarrierRun.java
@@ -0,0 +1,29 @@
+package org.rayson.thread.cyclicBarrierDemo;
+
+/***
+ *  Created with IntelliJ IDEA.
+ *  User:  lirui
+ *  Date:  2018-10-01
+ *  Time: 下午2:43
+ *  Description:
+ **/
+
+public class BarrierRun implements Runnable {
+    private boolean isTaskFinished;
+    private int N;
+
+    BarrierRun(boolean isTaskFinished, int n) {
+        this.isTaskFinished = isTaskFinished;
+        this.N = n;
+    }
+
+    @Override
+    public void run() {
+        if (isTaskFinished) {
+            System.out.println("司令：[士兵" + N + "个，任务完成！]");
+        } else {
+            System.out.println("司令：[士兵" + N + "个，集合完毕！]");
+            isTaskFinished = true;
+        }
+    }
+}
