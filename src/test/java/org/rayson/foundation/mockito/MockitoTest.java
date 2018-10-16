70dcac7895e53b32ff6016415ae742eb4acbe8ed
lirui <634178463@qq.com>
2018-10-16 21:43:55 +0800

    mock

---
+++ src/test/java/org/rayson/foundation/mockito/MockitoTest.java
@@ -0,0 +1,45 @@
+package org.rayson.foundation.mockito;
+
+import org.junit.Test;
+import org.rayson.foundation.annotations.Scenario;
+
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+/***
+ *  Created with IntelliJ IDEA.
+ *  User:  lirui
+ *  Date:  2018-10-16
+ *  Time: 下午6:15
+ *  Description:
+ **/
+public class MockitoTest {
+
+    @Scenario("一个对象的具体行为，通过 mock 实现")
+    @Test
+    public void testDemo1() {
+        LinkedList mockedLinkedList = mock(LinkedList.class);
+
+        when(mockedLinkedList.get(0)).thenReturn("100");
+        assertEquals("100", mockedLinkedList.get(0));
+
+        when(mockedLinkedList.get(anyInt())).thenReturn("000");
+        assertEquals("000", mockedLinkedList.get(12437));
+
+    }
+
+    @Scenario("创建一个对象的 spy，具有该对象的真实行为")
+    @Test
+    public void testSpy() {
+        LinkedList mockedList = spy(LinkedList.class);
+        mockedList.add("林黛玉");
+        mockedList.add("史湘云");
+        assertEquals("林黛玉", mockedList.get(0));
+        assertEquals("史湘云", mockedList.get(1));
+    }
+}
