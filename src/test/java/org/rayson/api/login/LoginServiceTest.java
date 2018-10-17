babf1db1a91e87ffe7182d016d531126cc772d3a
lirui <634178463@qq.com>
2018-10-17 17:07:33 +0800

    测试用户密码输入错误

--- src/test/java/org/rayson/api/login/LoginServiceTest.java
+++ src/test/java/org/rayson/api/login/LoginServiceTest.java
@@ -8,6 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.rayson.api.foundation.exception.BusinessException;
+import org.rayson.api.user.User;
 import org.rayson.api.user.UserRepository;
 
 import static org.mockito.Mockito.when;
@@ -41,5 +42,22 @@ public class LoginServiceTest {
         when(userRepository.getByUsername(noneExistUsername)).thenReturn(null);
 
         loginService.authenticate(noneExistUsername, password);
+    }
+
+    @Test
+    public void throws_BusinessException_for_password_or_username_incorrect() {
+        thrown.expect(BusinessException.class);
+        thrown.expectMessage(LoginCodes.USER_OR_PASSWORD_INCORRECT.message());
+
+        String username = "someone";
+        String password = "password";
+        User expectedUser = new User();
+        expectedUser.setUsername(username);
+        expectedUser.setPassword(password);
+
+        when(userRepository.getByUsername(username)).thenReturn(expectedUser);
+
+        String incorrectPassword = "incorrectPassword";
+        loginService.authenticate(username, incorrectPassword);
     }
 }
