16d6566567e2cb9dd4e4e59c639796db60cd305b
lirui <634178463@qq.com>
2018-12-25 16:19:21 +0800

    使用@Async 和 future 实现多任务并发、异步执行

--- src/main/java/org.rayson/RaysonApplication.java
+++ src/main/java/org.rayson/RaysonApplication.java
@@ -3,6 +3,7 @@ package org.rayson;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /***
@@ -15,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @MapperScan(basePackages = {"org.rayson"})
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class RaysonApplication {
     public static void main(String[] args) {
         SpringApplication.run(RaysonApplication.class, args);
